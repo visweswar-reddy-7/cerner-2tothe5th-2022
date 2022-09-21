0c2d6d592574e39e5bbfcf5317bf50ea16daebef
Sai Sreenath, Kalva <SaiSreenath.Kalva@cerner.com>
2022-09-21 14:09:29 +0530

    Day 7

---
+++ Day7/Animals.js
@@ -0,0 +1,30 @@
+// cerner-2tothe5th-2022
+
+// Abstract class
+class Animals {
+    constructor() {
+      if (this.constructor == Animals) {
+        throw new Error("Abstract classes can't be instantiated.");
+      }
+    }
+    sound() {
+        throw new Error("Method 'sound()' must be implemented.");
+    }
+}
+
+class Dog extends Animals {
+    sound() {
+        console.log("bark");
+    }
+}
+class Cat extends Animals {
+    sound() {
+        console.log("meow");
+    }
+}
+
+let cat = new Cat();
+let dog = new Dog();
+
+cat.sound();
+dog.sound();
