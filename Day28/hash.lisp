3710ce95345e828410f6dadaf7ba2f29f9f8f686
Visweswar Reddy <visweswarreddy29@gmail.com>
2022-10-12 10:40:34 +0530

    Day28 Lisp Maphash

---
+++ Day28/hash.lisp
@@ -0,0 +1,10 @@
+;; cerner_2tothe5th_2022
+
+;; creating maphash 
+(setq empList (make-hash-table))    
+
+(setf (gethash '001 empList) '(Charlie Brown))  
+(setf (gethash '002 empList) '(Freddie Seal)) 
+(setf (gethash '003 empList) '(Mark Mongoose)) 
+
+(maphash #'(lambda (k v) (format t "~a => ~a~%" k v)) empList)
