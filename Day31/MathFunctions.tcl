c7725d10166fa0b86d112968a7c3a04ecd6459bd
Visweswar Reddy <visweswarreddy29@gmail.com>
2022-10-15 20:49:46 +0530

    Day31 TCL mathfunctions

---
+++ Day31/MathFunctions.tcl
@@ -0,0 +1,24 @@
+#!cerner_2tothe5th_2022
+
+namespace import ::tcl::mathfunc::*
+
+# Tan
+puts [tan 10]
+
+# power
+puts [pow 10 2]
+
+# ceil
+puts [ceil 10.34]
+
+# hypothesis
+puts [hypot 10 20]
+
+# random
+puts [rand]
+
+# srand
+puts [srand 45]
+
+# log base 10
+puts [log10 10]
