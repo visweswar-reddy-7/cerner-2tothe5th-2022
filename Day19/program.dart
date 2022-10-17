3109ebda575e96e621681821ebedc6229fe4c311
Sai Sreenath, Kalva <SaiSreenath.Kalva@cerner.com>
2022-10-17 10:03:58 +0530

    Day 19

---
+++ Day19/program.dart
@@ -0,0 +1,18 @@
+// cerner_2tothe5th_2022
+import 'dart:isolate';  
+
+// concurrency method.
+void concurrency(var message){ 
+   print('execution from concurrency ... the message is :${message}'); 
+}  
+
+// execute concurrency method in different threads.
+void main(){
+   Isolate.spawn(concurrency,'Hello!!');
+   Isolate.spawn(concurrency,'Greetings!!');
+   Isolate.spawn(concurrency,'Welcome!!');
+
+   print('execution from main1');
+   print('execution from main2');
+   print('execution from main3');
+}
